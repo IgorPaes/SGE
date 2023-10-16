8ed3409e2736f935096fc81d62d170f5c139ccd2
Igor <igor.paes88@gmail.com>
2023-10-16 03:29:08 -0300

    feat: add múltiplas validações nos inputs p1.

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,3 @@
-var infosUser;
-
 function btnCadastrar() {
     
     let nome = document.querySelector('#nomeCadastro');
@@ -7,15 +5,64 @@ function btnCadastrar() {
     let senha = document.querySelector('#senhaCadastro');
     let cpf = document.querySelector('#cpfCadastro');
 
-    infosUser = [nome.value, email.value, senha.value, cpf.value]
+    let infosUser = [nome.value, email.value, senha.value, cpf.value];
 
-    nome.value = "";
-    email.value = "";
     senha.value = "";
-    cpf.value = "";
 
+    if(checkInputs(infosUser)) {
+        nome.value = "";
+        email.value = "";
+        cpf.value = "";
+        cadastrado();
+    }
+
+}
+
+function cadastrado() {
+    
     alert("Cadastrado!");
+    irLogin();
 
+}
+
+function irCadastro() {
+
+    const telaLogin = document.querySelector('#tela_login');
+    const telaCadastro = document.querySelector('#tela_cadastro');
+
+    telaLogin.style = "display: none;";
+    telaCadastro.style = "display: flex;";
+
+}
+
+function irLogin() {
+
+    const telaCadastro = document.querySelector('#tela_cadastro');
+    const telaLogin = document.querySelector('#tela_login');
+
+    telaCadastro.style = "display: none;";
+    telaLogin.style = "display: flex;";
+
+}
+
+function checkInputs(inputsAVerificar) {
+    
+    let inputsProntosVerificar;
+    inputsProntosVerificar = removerEspacos(inputsAVerificar);
+
+    const inputsAlterarEstiloCadastroAll = document.querySelectorAll('#InputTCadastro'); 
+
+    if(!verificarInputVazio(inputsProntosVerificar, inputsAlterarEstiloCadastroAll)) {
+        return false;
+    }
+
+    if(!verificarInputEmail(inputsProntosVerificar[1], inputsAlterarEstiloCadastroAll[1])) {
+        return false;
+    }
+
+    // return cpf.replace(/[^\d]/g, "");
+
+    return true;
 }
 
 function btnLogar() {
@@ -35,6 +82,44 @@ function btnLogar() {
 
 }
 
+function removerEspacos(inputsAVerificar) {
+
+    for(let i = 0; i < inputsAVerificar.length; i++) {
+        
+        inputsAVerificar[i] = (inputsAVerificar[i]).replace(/\s/g, "");
+    }
+
+    return inputsAVerificar;
+}
+
+function verificarInputVazio(inputsProntosVerificar, inputsAlterarEstilo) {
+    
+    let camposVerificados = true; 
+
+    for(let i = 0; i < inputsProntosVerificar.length; i++) {
+        if(inputsProntosVerificar[i] === '') {
+            inputsAlterarEstilo[i].style.borderColor = "red";
+            camposVerificados = false;
+        }else {
+            inputsAlterarEstilo[i].style.borderColor = "#3A4149";
+        }
+    }
+
+    return camposVerificados;
+}
+
+function verificarInputEmail(inputEmail, inputAlterarEstilo) {
+
+    if (inputEmail.indexOf("@") !== -1 && inputEmail.indexOf(".") !== -1) {  
+        inputAlterarEstilo.style.borderColor = "#3A4149";
+    }else {
+        inputAlterarEstilo.style.borderColor = "red";
+        return false;
+    }
+
+    return true;
+}
+
 function authLogin(emailLogin, senhaLogin) {
 
     const authEmail = emailLogin.value == infosUser[1] ? true : false;
@@ -44,4 +129,3 @@ function authLogin(emailLogin, senhaLogin) {
 
     return authFull;
 }
-
